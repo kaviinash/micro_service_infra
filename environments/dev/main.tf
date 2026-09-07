6bb5e27337e5b987ec56322975bd1d2f535c57c7
Avinash_Devops <kaviinash1@gmail.com>
2026-09-07 10:12:47 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "../../modules/resource-group"
+  source = "../../modules/resource_group"
 
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
