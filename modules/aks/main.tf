ac8fce21b3f9c7d159a278d406fae5a7c95fb665
Avinash_Devops <kaviinash1@gmail.com>
2026-09-07 11:15:51 +0530

    Change VM size in AKS default node pool

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = var.node_count
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard B2as v2"
   }
 
   identity {
