c91bd19810b63b69c5eb737d274fd5b8d72c88e1
Avinash_Devops <kaviinash1@gmail.com>
2026-09-07 10:07:31 +0530

    Update module source paths in main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "./modules/resource-group"
+  source = "../../modules/resource-group"
 
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
@@ -7,7 +7,7 @@ module "resource_group" {
 
 
 module "aks" {
-  source = "./modules/aks"
+  source = "../../modules/aks"
 
   aks_name            = var.aks_name
   location            = var.resource_group_location
@@ -17,7 +17,7 @@ module "aks" {
 
 
 module "acr" {
-  source = "./modules/acr"
+  source = "../../modules/acr"
 
   acr_name            = var.acr_name
   location            = var.resource_group_location
